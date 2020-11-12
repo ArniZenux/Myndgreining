24b07930dbf0cb7f9aa8b15b9877b3724d7c1192
Arni Ingi Johannesson <arni.ingi@skaginn3x.com>
2020-11-12 13:05:39 +0000

    #5 updated

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-"Einfald myndgreining" 
+"Einfald myndgreining á LeNet
 
 1) Installtion 
 
@@ -22,9 +22,9 @@
 4) Train.py 
 	Þjálfa myndum í deep learning og býr módel til.
 
-	python3 train.py 
+	python3 train.py --model fani.model --dataset data
 
 5) Test.py 
 	Keyra á OpenCV með mynd og láta að þekkja íslenska fána.
 
-	python3 test.py --model fani.model --image data/flag/image01.jpg 
+	python3 test.py --model fani.model --image data/IcelandicFlag/image01.jpg 
--- train.py
+++ train.py
@@ -1,6 +1,37 @@
 import numpy as np 
 from keras import backend as K
-
+import argparse
 
 train_dir = 'data/train'
 test_dir = 'data/test'
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--dataset", required=True,
+	help="path to input dataset")
+ap.add_argument("-m", "--model", required=True,
+	help="path to output model")
+ap.add_argument("-p", "--plot", type=str, default="plot.png",
+	help="path to output loss/accuracy plot")
+args = vars(ap.parse_args())
+
+train_datagen = ImageDataGenerator(
+	rescale = 1. /  255, 
+	shear_range = 0.2, 
+	zoom_range = 0.2, 
+	horizontal_flip = True)
+
+test_datagen = ImageDataGenerator(
+	rescale = 1. / 255)
+
+train_generator = train_datagen.flow_from_directory(
+	'data/train_little', 
+	target_size=(150,150), 
+	batch_size = batch_size, 
+	class_mode = 'binary')
+
+test_generator = test_datagen.flow_from_directory( 
+	'data/test_little', 
+	target_size = (150, 150), 
+	batch_size = batch_size, 
+	class_mode = 'binary')
